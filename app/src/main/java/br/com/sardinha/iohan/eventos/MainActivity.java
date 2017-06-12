e6c158d97d9316959d2c291a3c48e690d36f57bb
IohanSardinha <ioxsardinha@gmail.com>
2017-06-12 16:02:38 -0300

    Start adding login

--- app/src/main/java/br/com/sardinha/iohan/eventos/MainActivity.java
+++ app/src/main/java/br/com/sardinha/iohan/eventos/MainActivity.java
@@ -1,59 +1,68 @@
 package br.com.sardinha.iohan.eventos;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView eventos;
-    ItemListaEventosAdapter adapter;
+    private ProgressBar progress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        eventos = (ListView)findViewById(R.id.listaEventosMain);
-        adapter = new ItemListaEventosAdapter(this,R.layout.item_lista_eventos);
-        eventos.setAdapter(adapter);
-        eventos.setOnItemClickListener(new ListItemClick());
-    }
-    class ListItemClick implements AdapterView.OnItemClickListener{
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            ViewGroup vg = (ViewGroup) view;
-            ItemListaEventosAdapter tempAdapter = (ItemListaEventosAdapter)((ListView) parent).getAdapter();
-            Evento evento = (Evento) tempAdapter.getItem(position);
-            Intent intent = new Intent(MainActivity.this,DetalhesEventoActivity.class);
-            intent.putExtra("evento",evento);
-            startActivityForResult(intent,2);
-        }
+        progress = (ProgressBar)findViewById(R.id.progressBar1);
+        progress.setVisibility(View.GONE);
     }
 
-    public void novoEventoClick(View view) {
-        Intent intent = new Intent(this, NovoEventoActivity.class);
-        startActivityForResult(intent,1);
+    public void login(View view) {
+
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        progress.setVisibility(View.VISIBLE);
+        final String email = ((EditText)findViewById(R.id.email)).getText().toString();
+        final String pasword = ((EditText)findViewById(R.id.password)).getText().toString();
+        client.get("http://api-eventos.herokuapp.com/users", new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                progress.setVisibility(View.GONE);
+                Type listType = new TypeToken<List<User>>() {}.getType();
+                List<User> list;
+                Gson gson = new Gson();
+                list = gson.fromJson(new String(responseBody),listType);
+                for (User user:list){
+                    System.out.println(user.getEmail());
+                    System.out.println(user.getEmail());
+                    if(user.getEmail().equals(email) && user.getPassword().equals(pasword))
+                    {
+                        ((TextView)findViewById(R.id.result)).setText(user.getName());
+                    }
+                    else{
+                        ((TextView)findViewById(R.id.result)).setText("Email ou senha incorretos");
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                ((TextView)findViewById(R.id.result)).setText("Erro");
+                progress.setVisibility(View.GONE);
+            }
+        });
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1 && resultCode == RESULT_OK)
-        {
-            Evento evento = (Evento)data.getSerializableExtra("evento");
-            DBHandler db = new DBHandler(MainActivity.this);
-            db.addEvento(evento);
-            adapter.add(db.getEvento(evento.getLimite()));
-        }
-
-    }
-
 }
